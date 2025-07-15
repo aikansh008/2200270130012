3bbd4fb92f636a7f530ba693a08319455da7e2e9
Aikansh Tiwari <132533740+aikansh008@users.noreply.github.com>
2025-07-15 17:34:58 +0530

    Update Readme md\

--- Problem 2/services/register.js
+++ Problem 2/services/register.js
@@ -3,9 +3,9 @@ const axios = require('axios');
 const registerApp = async () => {
   try {
     await axios.post(process.env.REGISTER_URL, {
-      email: "vedanshi27vishu@gmail.com",
-      name: "vedanshi aggarwal",
-      rollNo: "2200270130193",
+      email: "aikanshtiwari007@gmail.com",
+      name: "aikansh tiwari",
+      rollNo: "2200270130012",
       accessCode: "uuMbyY",
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET
