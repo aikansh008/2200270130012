4487ed7b2b00a97831c97cf81611d88eb90bd0c9
Aikansh Tiwari <132533740+aikansh008@users.noreply.github.com>
2025-07-15 13:34:14 +0530

    Delete Problem2 directory

--- Problem2/middlewares/logger.js
+++
@@ -1,6 +0,0 @@
-const { logInfo } = require('../../LoggingMiddleware/middlewares/log.js');
-
-module.exports = (req, res, next) => {
-  logInfo("handler", `${req.method} ${req.originalUrl}`);
-  next();
-};
